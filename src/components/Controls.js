4427cf67784bd3ebce43bf5b568dd3c716ddd3f5
Araika <araikasingh@gmail.com>
2021-05-23 07:30:13 +0530

    ✨ added progress bar and seeking

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,13 +1,51 @@
 import React from "react";
+import { HiPlay, HiPause } from "react-icons/hi";
 
 const Controls = (props) => {
-  const { updatePlayerState, isPlaying } = props;
+  const { updatePlayerState, isPlaying, currentTime, duration } = props;
 
   return (
-    <div className="controls">
-      <button onClick={() => updatePlayerState({ isPlaying: !isPlaying })}>
-        {isPlaying ? "Pause" : "Play"}
-      </button>
+    <div className={`controls ${!isPlaying && "paused"}`}>
+      <div className="info">
+        {!isPlaying && (
+          <>
+            <h2>The Immigrant Song</h2>
+            <div>Led Zeppelin</div>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </>
+        )}
+      </div>
+
+      <div className="buttons">
+        <button onClick={() => updatePlayerState({ isPlaying: !isPlaying })}>
+          {isPlaying ? <HiPause /> : <HiPlay />}
+        </button>
+
+        <div className="custom-range">
+          <div className="progress-wrapper">
+            <div className="progress-bg"></div>
+            <div
+              className="progress-thumb"
+              style={{ left: `calc(${(currentTime / duration) * 100}% - 7px)` }}
+              // calc(variable% - 7px)
+            ></div>
+          </div>
+          <input
+            type="range"
+            min={0}
+            max={duration}
+            value={currentTime}
+            onChange={(event) =>
+              updatePlayerState({ currentTime: event.target.valueAsNumber })
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import ReactPlayer from "react-player";
 import video from "../assets/videos/song.mp4";
 
 const VideoPlayer = (props) => {
-  const { isPlaying } = props;
+  const { isPlaying, updatePlayerState, currentTime } = props;
+  const playerRef = useRef();
+
+  useEffect(() => {
+    if (!playerRef.current) return;
+    const delta = Math.abs(playerRef.current.getCurrentTime() - currentTime);
+    if (delta > 1) {
+      playerRef.current.seekTo(currentTime);
+    }
+  }, [currentTime]);
 
   return (
     <div className="player">
-      <ReactPlayer height="100%" width="100%" url={video} playing={isPlaying} />
+      <ReactPlayer
+        ref={playerRef}
+        height="100%"
+        width="100%"
+        url={video}
+        playing={isPlaying}
+        onDuration={(duration) => updatePlayerState({ duration })}
+        onProgress={(progress) =>
+          updatePlayerState({ currentTime: progress.playedSeconds })
+        }
+      />
     </div>
   );
 };
